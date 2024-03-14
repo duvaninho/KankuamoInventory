be379347b775c1c77da0122c65ad4d16ae7f539e
duvaninho <duvanguia@gmail.com>
2024-03-14 18:51:00 -0500

    some space

--- KankuamoInventory.Presentation/TechnologyEquipmentViewModel.cs
+++ KankuamoInventory.Presentation/TechnologyEquipmentViewModel.cs
@@ -1,41 +1,41 @@
-﻿using System.Collections.ObjectModel;
+﻿using KankuamoInventory.Core.Managers;
+using KankuamoInventory.Core.Models;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using KankuamoInventory.Core.Managers;
-using KankuamoInventory.Core.Models;
 namespace KankuamoInventory.Presentation;
 
 public class TechnologyEquipmentViewModel : INotifyPropertyChanged
 {
-  private ObservableCollection<TechnologyEquipmentModel> _equipmentRecords;
-    public ObservableCollection<TechnologyEquipmentModel> EquipmentRecords 
-    {
-        get => _equipmentRecords;
-        set
-        {
-            _equipmentRecords = value;
-            OnPropertyChanged();
-        }
-    }
+	private ObservableCollection<TechnologyEquipmentModel> _equipmentRecords;
+	public ObservableCollection<TechnologyEquipmentModel> EquipmentRecords
+	{
+		get => _equipmentRecords;
+		set
+		{
+			_equipmentRecords = value;
+			OnPropertyChanged();
+		}
+	}
 
-    private readonly ITechnologyEquipmentManager _equipmentService;
+	private readonly ITechnologyEquipmentManager _equipmentService;
 
-    public TechnologyEquipmentViewModel(ITechnologyEquipmentManager equipmentService)
-    {
-        _equipmentService = equipmentService;
-        LoadEquipmentRecords();
-    }
+	public TechnologyEquipmentViewModel(ITechnologyEquipmentManager equipmentService)
+	{
+		_equipmentService = equipmentService;
+		LoadEquipmentRecords();
+	}
 
-    public async Task LoadEquipmentRecords()
-    {
-        var technologyEquipments = await _equipmentService.GetTechnologyEquipmentsAsync();
-        EquipmentRecords = new ObservableCollection<TechnologyEquipmentModel>(technologyEquipments.Data);
-    }
+	public async Task LoadEquipmentRecords()
+	{
+		var technologyEquipments = await _equipmentService.GetTechnologyEquipmentsAsync();
+		EquipmentRecords = new ObservableCollection<TechnologyEquipmentModel>(technologyEquipments.Data);
+	}
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+	public event PropertyChangedEventHandler PropertyChanged;
+	protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
 }
